53d6abdd2431b10d1b4c34aa1a9cbc8ea0f73f3b
KipFeher85 <51102746+KipFeher85@users.noreply.github.com>
2019-11-08 13:04:30 -0500

    Add files via upload

--- WebScrape.py
+++ WebScrape.py
@@ -1,42 +1,61 @@
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
+import requests
 
-from basketball_reference_web_scraper import client
-# from basketball_reference_web_scraper.data import Team
-from basketball_reference_web_scraper.data import OutputType
+
+
 
 # NBA season we are analyzing
 year = 2019
 # url page we will be scrapping
-url = "https://www.basketball-reference.com/leagues/NBA_{}_per_game.html".format(year)
+url = requests.get('https://www.basketball-reference.com/leagues/NBA_2019_per_game.html')
+url2 = requests.get('https://www.basketball-reference.com/leagues/NBA_2020_per_game.html')
 # html object from the given URL
-html = urlopen(url)
-soup = BeautifulSoup(html, features="html.parser")
+soup = BeautifulSoup(url.text, features="html.parser")
+soup2 = BeautifulSoup(url2.text, features="html.parser")
+headers = []
 
-# Use findALL() to get the column headers
-soup.findAll('tr', limit=2)
-# Use getText to extract the text we need into a list
-headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
-# Exclude the first column, we dont need the rankings
-headers = headers[1:]
-headers
+
+def header_calculation(c):
+    headers_a = [th.getText() for th in c.findAll('tr', limit=2)[0].findAll('th')]
+    # Exclude the first column, we dont need the rankings
+    headers_a = headers_a[1:]
+    return headers_a
+
+
+# Get Column Headers
+def column_headers(a):
+    # Use findALL() to get the column headers
+    a.findAll('tr', limit=2)
+    headers_b = header_calculation(a)
+    print(headers_b)
+
+
+column_headers(soup)
+column_headers(soup2)
+
 
 # avoid the first row header
-rows = soup.findAll('tr')[1:]
-player_stats = [[td.getText() for td in rows[i].findAll('td')]
-                for i in range(len(rows))]
-stats = pd.DataFrame(player_stats, columns=headers)
-stats.head(10)
+def get_data(b):
+    rows = b.findAll('tr')[1:]
+    player_stats = [[td.getText() for td in rows[i].findAll('td')]
+                    for i in range(len(rows))]
+    headers_c = header_calculation(b)
+    stats = pd.DataFrame(player_stats, columns=headers_c)
+    print(stats.head(10))
 
 
-# Get play by play data for the 2018-10-16 game played at the Boston Celtics
+get_data(soup)
+get_data(soup2)
+
 """
-play_by_play = client.play_by_play(
-    home_team=Team.BOSTON_CELTICS,
-    year=2018,
-    month=10,
-    day=16,
-)
+print('****** TEAM BOX SCORES *******')
+team_box_scores = client.team_box_scores(day=1, month=1, year=2018)
+for total in team_box_scores[:10]:
+    print(total)
 """
+
+
+
+
 
